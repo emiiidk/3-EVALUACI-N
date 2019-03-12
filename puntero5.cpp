c5f7c582cd59a6c93085adc021ed9203f002304c
Emilio Estevan <43232464+emiiidk@users.noreply.github.com>
2019-03-12 09:40:53 +0100

    Update puntero5.cpp

--- puntero5.cpp
+++ puntero5.cpp
@@ -6,3 +6,5 @@ int main(){
     printf("x= %i",x);
     scanf("%c",&salir);
 }
+
+//Se asigna un valor a la variable "x" y luego se muestra por pantalla a traves de un printf, en el que se define el espacio de memoria para la "x" y se muestra como ya he comentado
